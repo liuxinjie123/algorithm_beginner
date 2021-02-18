34f677239a46c7c0b0e8879cb808e1f1b45212f8
1134005157 <1134005157@qq.com>
2021-02-18 16:42:45 +0800

    c x

---
+++ SortNew01.cpp
@@ -0,0 +1,32 @@
+/**
+ * ≈≈–Ú
+ *
+ * input
+ * 4
+ * 1582
+ *
+ * output
+ * 1258
+ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cout<<" c++ 2:"<<endl;
+    int n;
+    while(scanf("%d", &n) != EOF) {
+        int buf[n];
+        for (int i=0; i<n; i++) {
+            scanf("%d", &buf[i]);
+        }
+
+        sort(buf, buf+n);
+
+        for (int i=0; i<n; i++){
+            printf(" %d", buf[i]);
+        }
+        cout<<endl<<endl;
+        return 0;
+    }
+}
