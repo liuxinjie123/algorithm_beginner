bc54679364248d7426c2e9a9dedb0865383e8851
1134005157 <1134005157@qq.com>
2021-02-18 17:14:29 +0800

    c x

--- SortNew01.cpp
+++ SortNew01.cpp
@@ -12,6 +12,10 @@
 #include <algorithm>
 using namespace std;
 
+bool cmp (int x, int y) {
+    return x > y;
+}
+
 int main() {
     cout<<" c++ 2:"<<endl;
     int n;
@@ -22,6 +26,14 @@ int main() {
         }
 
         sort(buf, buf+n);
+
+        for (int i=0; i<n; i++){
+            printf(" %d", buf[i]);
+        }
+
+        cout<<endl<<endl;
+
+        sort(buf, buf+n, cmp);
 
         for (int i=0; i<n; i++){
             printf(" %d", buf[i]);
