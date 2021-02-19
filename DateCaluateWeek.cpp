ce16f3a76abf23d0c94f5b6f6b7dd90cbf94c88c
1134005157 <1134005157@qq.com>
2021-02-19 17:21:35 +0800

    c x

--- DateCaluateWeek.cpp
+++ DateCaluateWeek.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
-#define ISLEAPYEAR(n) n%4 == 0 && n%100 != 0 || n%400 == 0;
+#define ISLEAPYEAR(n) n%4 == 0 && n%100 != 0 || n%400 == 0
 
 int dayOfMonth[13][2] = {
 0,0,
@@ -30,7 +31,7 @@ struct Date {
 
     void nextDay() {
         Day++;
-        if (Day > dayOfMonth(Month, ISLEAPYEAR(Year))) {
+        if (Day > dayOfMonth[Month][ISLEAPYEAR(Year)]) {
             Day = 1;
             Month++;
             if (Month > 12) {
@@ -69,7 +70,38 @@ char weekName[7][20] = {
     "Saturday"
 };
 
+int main() {
+    Date tmp;
+    tmp.Year = 0;
+    tmp.Month = 1;
+    tmp.Day = 1;
 
+    int count = 0;
+
+    while(tmp.Year < 3001) {
+        buf[tmp.Year][tmp.Month][tmp.Day] = count;
+        tmp.nextDay();
+        count++;
+    }
+
+    int day, month, year;
+    char mName[20];
+
+    while (scanf("%d%s%d", &day, mName, &year)) {
+        for (month=1; month<=12; month++) {
+            if (strcmp(mName, monthName[month]) == 0) {
+                break;
+            }
+        }
+        printf("%d, %d, %d", year, month, day);
+        int days = buf[year][month][day] - buf[2021][2][19];
+        // 今天为星期五，加5
+        days += 5;
+        printf("%s\n\n", weekName[(days % 7 + 7) % 7]);
+        return 0;
+    }
+    return 0;
+}
 
 
 
